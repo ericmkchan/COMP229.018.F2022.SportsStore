18b95b79650e774947f6e7a6b1e7bd37712bfdd6
ericmkchan <113152408+ericmkchan@users.noreply.github.com>
2022-11-05 21:59:44 -0400

    0.3

--- SportsStore/src/app/model/product.repository.ts
+++ SportsStore/src/app/model/product.repository.ts
@@ -11,18 +11,18 @@ export class ProductRepository {
     constructor(private dataSource: StaticDataSource) {
         dataSource.getProducts().subscribe(data => {
             this.products = data;
-            this.categories = data.map(p => p.category)
+            this.categories = data.map(p => p.category || "")
                 .filter((c, index, array) => array.indexOf(c) == index).sort();
         });
     }
 
-    getProducts(category: string = null): Product[] {
+    getProducts(category: string = ""): Product[] {
         return this.products
-            .filter(p => category == null || category == p.category);
+            .filter(p => category == "" || category == p.category);
     }
 
     getProduct(id: number): Product {
-        return this.products.find(p => p.id == id);
+        return (this.products.find(p => p.id == id)!);
     }
 
     getCategories(): string[] {
